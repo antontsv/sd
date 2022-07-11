fdb94f29967aa3cbda2009cbdd894038e47cbf1f
Anton Tsviatkou <a.tsviatkou@gmail.com>
2022-07-11 00:36:06 -0700

    Add Top K and rate limiter sketches

--- includes/js/card-data.js
+++ includes/js/card-data.js
@@ -4,4 +4,14 @@ var cards = [
         "description": "Distributed Message Queue where any particular message processed by one consumer only",
         "url": "./data/Point-to-Point-Message-Queue.svg" 
     },
+    {
+        "name": "Top K heavy hitters",
+        "description": "Top K twits, videos, trending searches etc",
+        "url": "./data/Top-K-heavy-hitters.svg" 
+    },
+    {
+        "name": "Rate Limiter",
+        "description": "Rate limiter component to protect resource/service from overloading",
+        "url": "./data/Rate-Limiter.svg" 
+    },
 ]
