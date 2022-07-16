5acc06e88e71e734622f9774e24d7ac2ca6e6f3c
Anton Tsviatkou <a.tsviatkou@gmail.com>
2022-07-15 23:36:08 -0700

    Reformat using new exporter

--- includes/js/card-data.js
+++ includes/js/card-data.js
@@ -2,32 +2,32 @@ var cards = [
     {
         "name": "Point-to-Point Message Queue",
         "description": "Distributed Message Queue where any particular message processed by one consumer only",
-        "url": "./data/Point-to-Point-Message-Queue.svg" 
+        "url": "./data/Point-to-Point-Message-Queue.svg"
     },
     {
         "name": "Top K heavy hitters",
         "description": "Top K twits, videos, trending searches etc",
-        "url": "./data/Top-K-heavy-hitters.svg" 
+        "url": "./data/Top-K-heavy-hitters.svg"
     },
     {
         "name": "Rate Limiter",
         "description": "Rate limiter component to protect resource/service from overloading",
-        "url": "./data/Rate-Limiter.svg" 
+        "url": "./data/Rate-Limiter.svg"
     },
     {
         "name": "Distributed Message Queue",
         "description": "Modern message queue using brokers",
-        "url": "./data/Distributed-Message-Queue.svg" 
+        "url": "./data/Distributed-Message-Queue.svg"
     },
     {
         "name": "Notification service",
         "description": "Service that notifies users about events through many channels like email, mobile, webhooks",
-        "url": "./data/Notification-Service.svg" 
+        "url": "./data/Notification-Service.svg"
     },
     {
         "name": "Replication",
         "description": "Short summary of Designing Data-Intensive Applications (chapter 6) by Martin Kleppman",
-        "url": "./data/Replication.svg" 
+        "url": "./data/Replication.svg"
     },
     {
         "name": "Distributed cache",
